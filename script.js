9b5d75bb06cfa65358b6edf06c927b9255ddb6fe
Hamza <khanzadahamza76>
2024-10-16 04:26:55 -0700

    add js

---
+++ script.js
@@ -0,0 +1,79 @@
+let js = "amazing";
+console.log(55+10);
+
+// values and variables
+console.log("jonas");
+console.log(34);
+
+let firstName = "Matlida";
+console.log(firstName);
+
+// variable names must be create letters , underscores or the dollar sign
+
+let last_Name = "khan";
+
+// reserved key words means already define in javascript
+
+// let new = 6;
+
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+console.log(myFirstJob);
+
+let country = "Pakistan" ;
+console.log( typeof country);
+
+let continent = "Asia";
+console.log(typeof continent);
+
+let population = 235.8
+console.log(typeof population)
+
+
+// Data Types in Javascript 
+
+// There are 7 datatypes
+
+//  1. Number: Floating point numbers 👉 Used for decimal and numbers  let age = 23;
+//  2. String: Squence of Characters 👉 Used for text let firstName = "jonas";
+//  3. Boolean: Logical type that can be true or false 👉 Used for taking decisions let fullAge = true;
+//  4 undefined: Value taken by a Variable that is not yet define (empty value) like this let children;
+//  5 Null: Also means Empty Value
+//  6 Symbol (Es2015): Value that a unique and cannot be changed [Not useful for now]
+//  7 Bigint (Es2020): Larger integers than the number type can hold
+
+     
+    let javascriptIsFun = true;
+    console.log(javascriptIsFun);
+
+    console.log(typeof true);
+    console.log(typeof 23);
+    console.log(typeof "javascript");
+    javascriptIsFun = "yes!";
+    console.log(typeof javascriptIsFun);
+
+    let year;
+    console.log(year);
+    console.log(typeof year);
+
+    year = 1991;
+    console.log(typeof year);
+
+    let isIsland = "pakistan";
+    isIsland = true;
+    console.log(typeof isIsland)
+    let language;
+    console.log(typeof language);
+
+
+
+
+
+
+
+
